48825ff3d05dd8d67b48e1fefc2dadfc1519adff
mchowdhury2023 <chowdhury7135@saskpolytech.ca>
2024-02-08 01:19:25 -0600

    worked on fizzbuzz function

--- day-17/main.py
+++ day-17/main.py
@@ -1,8 +1,25 @@
 class User:
-    def __init__(self, fname,lname ):
-        self.fname = fname
-        self.lname = lname
+    def __init__(self, id,username ):
+        self.id = id
+        self.username = username
+        self.follower = 0
+        self.following = 0
+    def follow(self, user):
+        user.follower += 1
+        self.following += 1
 
-user_1 = User("Roney", "Chowdhury")
+user_1 = User("01", "Roney Chowdhury")
+user_2 = User("02", "Mahamudul Chowdhury")
 
-print(user_1.lname)
+print("before following")
+print(user_1.follower)
+print(user_1.following)
+print(user_2.follower)
+print(user_2.following)
+
+user_1.follow(user_2)
+print("After following")
+print(user_1.follower)
+print(user_1.following)
+print(user_2.follower)
+print(user_2.following)
---
+++ day-17/quiz-game-start/main.py
@@ -0,0 +1,21 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+#print(question_bank[0].text)
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+#print("\n")
+print("\nYou have completed the Quiz")
+print(f"Your final score is: {quiz.score}/ {quiz.question_number}")
